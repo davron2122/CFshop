9dac52cdbac99fe73b78a3e609bbadd1a0384613
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-04-13 03:16:11 +0900

    initial commit (BaseActivity,BaseViewModel)

---
+++ app/src/main/java/com/example/cfshopping/base/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.example.cfshopping.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
+
+public abstract class BaseActivity <VB extends ViewBinding, VM extends BaseViewModel> extends AppCompatActivity {
+
+    public  VB binding;
+    public VM viewModel;
+
+    protected abstract  VB inflateView(LayoutInflater inflater);
+    protected abstract  VM getViewModel();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=inflateView(getLayoutInflater());
+        setContentView(binding.getRoot());
+        viewModel=getViewModel();
+    }
+}
---
+++ app/src/main/java/com/example/cfshopping/base/BaseViewModel.java
@@ -0,0 +1,6 @@
+package com.example.cfshopping.base;
+
+import androidx.lifecycle.ViewModel;
+
+public abstract class BaseViewModel extends ViewModel {
+}
