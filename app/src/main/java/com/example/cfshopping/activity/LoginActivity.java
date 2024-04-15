a72c3b6b3dd3c1b0b61f8c3bbf354f16dc7506ce
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-04-16 00:31:07 +0900

    added some drawables, colors, string amd created activity_login.xml

--- app/src/main/java/com/example/cfshopping/activity/LoginActivity.java
+++ app/src/main/java/com/example/cfshopping/activity/LoginActivity.java
@@ -1,4 +1,21 @@
 package com.example.cfshopping.activity;
 
-public class LoginActivity {
+import android.view.LayoutInflater;
+
+import com.example.cfshopping.base.BaseActivity;
+import com.example.cfshopping.base.BaseNavigator;
+import com.example.cfshopping.base.BaseViewModel;
+import com.example.cfshopping.databinding.ActivityLoginBinding;
+import com.example.cfshopping.navigator.LoginNavigator;
+
+public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements LoginNavigator {
+    @Override
+    protected ActivityLoginBinding inflateView(LayoutInflater inflater) {
+        return ActivityLoginBinding.inflate(inflater);
+    }
+
+    @Override
+    protected BaseViewModel getViewModel() {
+        return null;
+    }
 }
---
+++ app/src/main/java/com/example/cfshopping/navigator/LoginNavigator.java
@@ -0,0 +1,6 @@
+package com.example.cfshopping.navigator;
+
+import com.example.cfshopping.base.BaseNavigator;
+
+public interface LoginNavigator extends BaseNavigator {
+}
