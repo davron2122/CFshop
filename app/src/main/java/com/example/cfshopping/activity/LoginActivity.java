4cf58265e74c5511f706b501fa238ec95e9287fc
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-04-15 21:30:40 +0900

    creating SplashActivity.java, LoginActivity.java, SignUpActivity.java

---
+++ app/src/main/java/com/example/cfshopping/activity/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.cfshopping.activity;
+
+public class LoginActivity {
+}
---
+++ app/src/main/java/com/example/cfshopping/activity/SplashActivity.java
@@ -0,0 +1,6 @@
+package com.example.cfshopping.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+}
