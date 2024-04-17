4d6b3b270229d7dc555d232913666c3410372059
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-04-18 01:04:41 +0900

    added database Room

---
+++ app/src/main/java/com/example/cfshopping/db/AppDatabase.java
@@ -0,0 +1,13 @@
+package com.example.cfshopping.db;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.cfshopping.model.User;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+
+}
