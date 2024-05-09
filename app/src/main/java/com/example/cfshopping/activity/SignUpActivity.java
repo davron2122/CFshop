63e47b1e44a1a8eb60ad280a3e0dcddba60b7af2
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-05-10 02:14:06 +0900

    created SignUpNavigator.java

--- app/src/main/java/com/example/cfshopping/activity/SignUpActivity.java
+++ app/src/main/java/com/example/cfshopping/activity/SignUpActivity.java
@@ -1,4 +1,20 @@
 package com.example.cfshopping.activity;
 
-public class SignUpActivity {
+import android.view.LayoutInflater;
+
+import androidx.viewbinding.ViewBinding;
+
+import com.example.cfshopping.base.BaseActivity;
+import com.example.cfshopping.base.BaseViewModel;
+
+public class SignUpActivity extends BaseActivity {
+    @Override
+    protected ViewBinding inflateView(LayoutInflater inflater) {
+        return null;
+    }
+
+    @Override
+    protected BaseViewModel getViewModel() {
+        return null;
+    }
 }
